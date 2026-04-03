dcb65433f13460f2b994651b2eadef385c5e0da0
SanthuSreekumar <santhusreekumar1234@gmail.com>
2026-04-03 21:35:32 +0000

    Version 6: Searches for specfic cmds

--- bashstats.py
+++ bashstats.py
@@ -19,10 +19,16 @@ for line in clean_lines:
     first_word=line.split()[0]
     commands.append(first_word)
 
-command_count = Counter(commands)                                     # counter is easier and simpler 
+command_count = Counter(commands)                                     # counter is easier and simpler
 
 print("Ten most used commands- \n")
 
 for command, count in command_count.most_common(10):                   #(10) gives the top 10 while ()[10] gives us the 11th unit in the list
     print(command, ":" ,count)
 
+search_command = input("\nEnter the command you want to search : ")     #search for specific command
+
+if search_command in command_count:
+    print(search_command,":",command_count[search_command])
+else:
+    print("Command not found")
