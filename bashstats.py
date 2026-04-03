1029618ea18f3c770b93a90d68ceb13ea8a3c715
SanthuSreekumar <santhusreekumar1234@gmail.com>
2026-04-03 12:29:02 +0000

    Version 4: Command counts

--- bashstats.py
+++ bashstats.py
@@ -17,5 +17,13 @@ for line in clean_lines:
     first_word=line.split()[0]
     commands.append(first_word)
 
-print("Total commands used : ", len(commands))
+command_count = {}                                         #counts the commands  #{}-Dictionary-counts the cmds in the list
+
+for command in commands:
+    if command in command_count:
+        command_count[command] += 1
+    else:
+        command_count[command] = 1
+
+print("Total commands used : ", command_count)
 
